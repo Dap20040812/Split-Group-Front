9c641bcedceb089ee1bf7a3fd9e9b91ffa49d800
Diego Prado <diegoprpr@unisabana.edu.co>
2023-10-15 04:17:13 -0500

    set user login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,26 @@
+import React, {useEffect} from 'react';
+import getUser from './query/loginClaro';
+import {useDispatch} from "react-redux"
+import {setUserLogin} from './features/user/UserSlice'
 function App() {
+
+  const dispatch = useDispatch()
+
+
+  useEffect(() => {
+    const setUser = async () => {
+      var response = await getUser();
+      dispatch(setUserLogin({
+        name: response.name,
+        uid: response.id,       
+      }))
+      console.log(response);
+    }
+
+    setUser();
+  }, []);
+
+
   return (
     <div className="App">
 
