e98b956a3f358e8149f06087aa951dbf6a9d919c
SebastianGuevara <sebas.guevara.velez@gmail.com>
2023-10-15 13:11:14 -0500

    CAMBIA HOST

--- src/api-constrains.js
+++ src/api-constrains.js
@@ -1,3 +1,3 @@
-const urlApi = 'https://07pn30fas5.execute-api.us-east-1.amazonaws.com/prod';
+const urlApi = 'https://a0oald95l0.execute-api.us-east-1.amazonaws.com/prod';
 
 export default urlApi;
--- src/components/features/Analysis.jsx
+++ src/components/features/Analysis.jsx
@@ -4,9 +4,9 @@ import { MovementContainer } from '../common/Container'
 const Analysis = () => {
     return(
         <MovementContainer style={{display:"flex", alignItems:"center"}}>
-            <h4 style={{color:"#FFFFA7"}}>Gastos e Ingresos diarios por Categoría</h4>
+            <h4 style={{color:"#FFFFFF"}}>Gastos e Ingresos diarios por Categoría</h4>
             <img alt="1" src='1.png' height="300px"/>
-            <h4 style={{color:"#FFFFA7"}}>Volumen de Gastos Mensuales por Categoía</h4>
+            <h4 style={{color:"#FFFFFF"}}>Volumen de Gastos Mensuales por Categoía</h4>
             <img alt="1" src='2.png' height="300px"/>
         </MovementContainer>
     )
